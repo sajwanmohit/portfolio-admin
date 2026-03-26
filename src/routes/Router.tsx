0439ad9fcf06ad1ff2fc9512505fba9a9e97bc76
Mohit Sajwan <mohitsajwan0@gmail.com>
2026-03-26 09:32:48 +0530

    fix : removed unused Dashboard import

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import AdminLayout from "../components/layout/AdminLayout";
-import Dashboard from "../pages/Dashboard";
 import Projects from "../pages/Projects";
 import Login from "../pages/Login";
 import Skills from "../pages/Skills";
